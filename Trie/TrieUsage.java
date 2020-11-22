5a94c6dc50f5f9f2f0de473aa05c99cd863bef2f
CodePrometheus <Code_Prince@outlook.com>
2020-11-22 20:30:16 +0800

    QuickFind

--- Trie/TrieUsage.java
+++ Trie/TrieUsage.java
@@ -1,7 +1,7 @@
 import java.util.TreeMap;
 
 /**
- * 实现字典树
+ * 实现字典树  又叫前缀树
  *
  * @Author: zzStar
  * @Date: 11-05-2020 22:28
@@ -42,8 +42,10 @@ public class TrieUsage {
             cur = cur.next.get(c);
         }
 
-        if (!cur.isWord) cur.isWord = true;
-        size++;
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
     }
 
 
@@ -51,12 +53,31 @@ public class TrieUsage {
     public boolean contains(String word) {
         Node cur = root;
         for (int i = 0; i < word.length(); i++) {
+            // 每遍历到的都转成一个char
             char c = word.charAt(i);
+            // 没有该word这整个字符串
             if (cur.next.get(c) == null)
                 return false;
             cur = cur.next.get(c);
         }
+        // 最终要判断是否匹配这个单词，而非直接return true
         return cur.isWord;
+    }
+
+
+    // 是否有与指定前缀匹配的单词
+    public boolean inPrefix(String prefix) {
+        Node cur = root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+            // 每遍历到的都转成一个char
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        // 这里就直接return true
+        return true;
     }
 
 }
---
+++ UnoinFind/UF.java
@@ -0,0 +1,14 @@
+/**
+ * @Description: 并查集接口实现
+ * @Author: zzStar
+ * @Date: 2020/11/22 20:14
+ */
+public interface UF {
+
+    boolean isConnected(int p, int q);
+
+    void unionElements(int p, int q);
+
+    int getSize();
+
+}
